10526ea242f4c78ca3ae094893dab564e4422e05
jiarunlu0701 <jiarun.lu@newsbreak.com>
2023-06-05 00:26:51 -0700

    added sound, but it all go at once, every annoying, solve it next day.

--- Coaching.py
+++ Coaching.py
@@ -1,5 +1,7 @@
 import openai
 import time
+from playsound import playsound
+
 openai.api_key = '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'
 
 class Gpt4Coaching:
@@ -13,6 +15,14 @@ class Gpt4Coaching:
             'stance': None,
             'depth': None,
             'speed': None
+        }
+        self.issued_warnings = {
+            'ankle': False,
+            'center': False,
+            'knee_intorsion': False,
+            'stance': False,
+            'depth': False,
+            'speed': False
         }
 
     def analyze_metrics(self, movement, metrics):
@@ -32,6 +42,7 @@ class Gpt4Coaching:
             if self.last_warning_time['ankle'] is None or current_time - self.last_warning_time['ankle'] >= self.warning_cooldown:
                 warning = "Warning: Your knee is passing the front toe. Try to maintain a proper form.\n"
                 self.warn_user(warning, 'ankle', current_time)
+                self.issued_warnings['ankle'] = False
 
     def check_centered_squat(self, metrics):
         check_lowest_list = metrics.get('check_lowest', [])
@@ -43,6 +54,7 @@ class Gpt4Coaching:
                 if self.last_warning_time['center'] is None or action_time - self.last_warning_time['center'] >= self.warning_cooldown:
                     warning = f"Warning: You are not centered. You are {calculate_center}. Try to maintain a proper form.\n"
                     self.warn_user(warning, 'center', action_time)
+                    self.issued_warnings['center'] = False
 
     def check_knee_intorsion(self, metrics):
         check_lowest_list = metrics.get('check_lowest', [])
@@ -54,6 +66,7 @@ class Gpt4Coaching:
                 if self.last_warning_time['knee_intorsion'] is None or action_time - self.last_warning_time['knee_intorsion'] >= self.warning_cooldown:
                     warning = f"Warning: Possible knee intorsion detected. Try to maintain a proper form.\n"
                     self.warn_user(warning, 'knee_intorsion', action_time)
+                    self.issued_warnings['knee_intorsion'] = False
 
     def check_stance_width(self, metrics):
         current_time = time.time()
@@ -71,10 +84,11 @@ class Gpt4Coaching:
             last_action = check_lowest_list[-1]
             knee_angle = last_action[0]
             action_time = last_action[1]
-            if knee_angle <= 90:  # Assuming a good squat has knee_angle <= 90
+            if knee_angle <=90:  # Assuming a good squat has knee_angle <= 90
                 if self.last_warning_time['depth'] is None or action_time - self.last_warning_time['depth'] >= self.warning_cooldown:
                     warning = f"Warning: Your squat is shallow (knee angle: {knee_angle}). Try to reach at least a 90 degree knee angle.\n"
                     self.warn_user(warning, 'depth', action_time)
+                    self.issued_warnings['depth'] = False
 
     def check_squat_speed(self, metrics):
         check_lowest_list = metrics.get('check_lowest', [])
@@ -86,13 +100,32 @@ class Gpt4Coaching:
                     self.last_warning_time['speed'] is None or last_squat_time - self.last_warning_time['speed'] >= self.warning_cooldown):
                 warning = f"Warning: You are squatting too fast. Try to take at least 2 seconds per squat.\n"
                 self.warn_user(warning, 'speed', last_squat_time)
+                self.issued_warnings['speed'] = False
 
     def warn_user(self, warning, warning_type, time):
-        #print(self.feedback)
-        print(warning)
-        self.last_warning_time[warning_type] = time  # Update the time when the warning was given
-        if warning not in self.feedback:
-            self.feedback += warning
+        if not self.issued_warnings[warning_type]:
+            if warning_type == 'ankle':
+                playsound('ankle.wav', block=False)
+
+            elif warning_type == 'center':
+                playsound('center.wav', block=False)
+
+            elif warning_type == 'knee_intorsion':
+                playsound('knee_intorsion.wav', block=False)
+
+            elif warning_type == 'stance':
+                playsound('stance.wav', block=False)
+
+            elif warning_type == 'depth':
+                playsound('depth.wav', block=False)
+
+            elif warning_type == 'speed':
+                playsound('speed.wav', block=False)
+
+            self.issued_warnings[warning_type] = True
+        elif time - self.last_warning_time[warning_type] >= self.warning_cooldown:
+            self.issued_warnings[warning_type] = False  # Reset only after cooldown period has passed
+        self.last_warning_time[warning_type] = time
 
     def generate_chat(self, prompt):
         self.messages.append({"role": "user", "content": prompt})
